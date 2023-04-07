e9341490d77d9fff9f76dd2a26897d61af12fc10
andreeaboth <both.andreea@gmail.com>
2023-04-07 13:38:52 +0300

    test issue

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val equals = test.equals("")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
